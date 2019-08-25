d540b00f1868c3e0a94da5b942af8361723dff41
Jeremy Manalo <jeremy.h.manalo@gmail.com>
2019-08-25 12:18:50 -0700

    now has cs files

---
+++ InfiniteMeals/InfiniteMeals/Kitchens/Controller/MainPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using InfiniteMeals.Kitchens.Model;
+using System.Collections.ObjectModel;
+
+namespace InfiniteMeals
+{
+    public partial class MainPage : ContentPage
+    {
+
+        ObservableCollection<KitchensModel> Kitchens = new ObservableCollection<KitchensModel>();
+
+        protected async void GetKitchens()
+        {
+            var request = new HttpRequestMessage();
+            request.RequestUri = new Uri("https://phaqvwjbw6.execute-api.us-west-1.amazonaws.com/dev/api/v1/kitchens");
+            request.Method = HttpMethod.Get;
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                HttpContent content = response.Content;
+                var kitchensString = await content.ReadAsStringAsync();
+                JObject kitchens = JObject.Parse(kitchensString);
+                this.Kitchens.Clear();
+
+                foreach (var k in kitchens["result"])
+                {
+                    this.Kitchens.Add(new KitchensModel()
+                    {
+                        kitchen_id = k["kitchen_id"]["S"].ToString(),
+                        title = k["kitchen_name"]["S"].ToString(),
+                        close_time = k["close_time"]["S"].ToString(),
+                        description = k["description"]["S"].ToString(),
+                        open_time = k["open_time"]["S"].ToString(),
+                        isOpen = (Boolean)k["isOpen"]["BOOL"],
+                        status = ((Boolean)k["isOpen"]["BOOL"] == true) ? "Open now" : "Closed",
+                        statusColor = ((Boolean)k["isOpen"]["BOOL"] == true) ? "Green" : "Red",
+                        opacity = ((Boolean)k["isOpen"]["BOOL"] == true) ? "1.0" : "0.6"
+                    }
+                    );
+                }
+
+                kitchensListView.ItemsSource = Kitchens;
+            }
+
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            GetKitchens();
+
+            //Kitchens.Clear();
+
+            kitchensListView.RefreshCommand = new Command(() =>
+            {
+               GetKitchens();
+               kitchensListView.IsRefreshing = false;
+            });
+
+            kitchensListView.ItemSelected += Handle_ItemTapped();
+
+        }
+
+        private EventHandler<SelectedItemChangedEventArgs> Handle_ItemTapped()
+        {
+            return OnItemSelected;
+        }
+
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            // disable selected item highlighting;
+            if (e.SelectedItem == null) return;
+            ((ListView)sender).SelectedItem = null;
+
+
+            // do something with the selection
+            var kitchen = e.SelectedItem as KitchensModel;
+
+            // disable selection if the kitchen is closed
+            if (kitchen.isOpen == false)
+            {
+                return;
+            }
+
+            await Navigation.PushAsync(new SelectMealOptions(kitchen.kitchen_id));
+        }
+    }
+}
---
+++ InfiniteMeals/InfiniteMeals/Kitchens/Model/KitchensModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace InfiniteMeals.Kitchens.Model
+{
+    public class KitchensModel
+    {
+        public string kitchen_id { get; set; }
+        public string title { get; set; }
+        public string close_time { get; set; }
+        public string description { get; set; }
+        public string open_time { get; set; }
+        public bool isOpen { get; set; }
+        public string status { get; set; }
+        public string statusColor { get; set; }
+        public string opacity { get; set; }
+    }
+}
