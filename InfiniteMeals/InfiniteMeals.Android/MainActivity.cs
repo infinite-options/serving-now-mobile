17a8b09ee8080738d8e21319f2ec1085ebe89968
AjaipalSinghChhina <chhinaromy@gmail.com>
2020-04-27 17:32:35 -0700

    Added few things in Mainpage

--- InfiniteMeals/InfiniteMeals.Android/MainActivity.cs
+++ InfiniteMeals/InfiniteMeals.Android/MainActivity.cs
@@ -10,6 +10,7 @@ using Android.OS;
 namespace InfiniteMeals.Droid
 {
     [Activity(Label = "Serving Now", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    //[Activity(Label = "@string/app_name", Theme = "@style/Theme.AppCompat.Light.NoActionBar", MainLauncher = true)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
